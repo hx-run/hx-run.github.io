2a8f0c8e4f63c9f4fc83a23d379ad5466bd35820
hx-run <info@principai.co>
2026-01-18 17:20:01 +0300

    fix: fix baseURL issue

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   url: 'https://hx-run.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/hx-run.github.io',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
